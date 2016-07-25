173e8a0c816f124ffc81ef71e646166826d4e4c5
wasanBaac <wasan_fc@hotmail.com>
2016-07-25 16:09:51 +0700

    Commit enter passcode

--- www/js/index_user_scripts.js
+++ www/js/index_user_scripts.js
@@ -16,12 +16,29 @@
 
 
         /* button  #btn1 */
+     var numClick=0;
     $(document).on("click", "#btn1", function(evt)
     {
         /* your code goes here */
-        document.getElementById("rdbInput1").checked = true;
-        document.getElementById("cb1").checked = true;
-        alert('11');
+        numClick = numClick+1;
+        if(numClick==1){
+            document.getElementById("btnChecked1").style.background='#FFFFFF';
+        }
+        if(numClick==2){
+            document.getElementById("btnChecked2").style.background='#FFFFFF';
+        }
+        if(numClick==3){
+            document.getElementById("btnChecked3").style.background='#FFFFFF';
+        }
+        if(numClick==4){
+            document.getElementById("btnChecked4").style.background='#FFFFFF';
+        }
+    });
+
+        /* button  #btn2 */
+    $(document).on("click", "#btn2", function(evt)
+    {
+        /* your code goes here */
     });
 
     }
